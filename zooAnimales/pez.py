f5a4db0eb62a7ece882d272ba9903cac66a4b7ab
lladinog <159490210+lladinog@users.noreply.github.com>
2024-03-19 12:22:30 -0500

    pato

---
+++ zooAnimales/pez.py
@@ -0,0 +1,35 @@
+from .animal import Animal
+
+class Pez(Animal):
+    totalPeces = 0
+    salmones = 0
+    bacalaos = 0
+
+    def __init__(self, nombre="", edad=0, habitat="", genero="", colorEscamas="", cantidadAletas=0):
+        super().__init__(nombre, edad, habitat, genero)
+        self.colorEscamas = colorEscamas
+        self.cantidadAletas = cantidadAletas
+        Pez.totalPeces += 1
+
+    @staticmethod
+    def crearBacalao(nombre, edad, genero):
+        Pez.bacalaos += 1
+        return Pez(nombre, edad, "oceano", genero, "gris", 6)
+
+    @staticmethod
+    def crearSalmon(nombre, edad, genero):
+        Pez.salmones += 1
+        return Pez(nombre, edad, "oceano", genero, "rojo", 6)
+
+    @staticmethod
+    def cantidadPeces():
+        return Pez.totalPeces
+
+    def movimiento(self):
+        return "nadar"
+
+    def getColorEscamas(self):
+        return self.colorEscamas
+
+    def getCantidadAletas(self):
+        return self.cantidadAletas
